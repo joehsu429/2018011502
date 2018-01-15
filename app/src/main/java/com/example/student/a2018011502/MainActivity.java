cf0fb24410b3a31aa0edd47751c0e8dbe06d4623
joe <kkwhsu@gmail.com>
2018-01-15 15:06:30 +0800

    011502-4

--- app/src/main/java/com/example/student/a2018011502/MainActivity.java
+++ app/src/main/java/com/example/student/a2018011502/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.student.a2018011502;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -19,15 +21,17 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
-    TextView tv;
-    ProgressBar pb;
+    TextView tv,tv2,tv3;
+    ProgressBar pb;//2 進度條顯示
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img=findViewById(R.id.imageView);
         tv=findViewById(R.id.textView);
-        pb=findViewById(R.id.progressBar);
+        tv2=findViewById(R.id.textView2);
+        tv3=findViewById(R.id.textView3);
+        pb=findViewById(R.id.progressBar);//2 進度條顯示
     }
     public void click1(View v)
     {
@@ -57,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
                                 tv.setText(String.valueOf(tmp)+"/"+totallength);
-                                pb.setProgress(100 * tmp / totallength);//進度條顯示
+                                pb.setProgress(1000 * tmp / totallength);//2 進度條顯示
                             }
                         });
                     }
@@ -81,5 +85,98 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
+    public void click2(View v)//3
+    {
+        MyTask task=new MyTask();//af1
+        task.execute(10);
+    }
+    class MyTask extends AsyncTask<Integer,Integer,String>
+    {
+        @Override
+        protected void onPostExecute(String s)//af1
+        {
+            super.onPostExecute(s);
+            tv3.setText(s);
+        }
 
+        @Override
+        protected void onProgressUpdate(Integer... values)//af1
+        {
+            super.onProgressUpdate(values);
+            tv2.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers)//af1 ..可輸入不確定數量的陣列,一定要放在最後面
+        {
+            int i;
+            for(i=0;i<=integers[0];i--)
+            {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("Task","doInBackground,i:"+i);
+                publishProgress(i);
+            }
+            return "wow";
+        }
+    }
+    public void click3(View v)//af2
+    {
+        MyImageTask task=new MyImageTask();
+        task.execute("https://5.imimg.com/data5/UH/ND/MY-4431270/red-rose-flower-500x500.jpg");
+    }
+    class MyImageTask extends AsyncTask<String,Integer,Bitmap>
+    {
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            img.setImageBitmap(bitmap);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String str_url="https://5.imimg.com/data5/UH/ND/MY-4431270/red-rose-flower-500x500.jpg";
+            URL url;
+            try{
+                url = new URL(str_url);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                InputStream inputStream=conn.getInputStream();
+                ByteArrayOutputStream bos=new ByteArrayOutputStream();
+                byte[] buf=new byte[1024];
+                final int totallength=conn.getContentLength();
+                int sum=0;
+                int length;
+                while((length=inputStream.read(buf)) !=-1)
+                {
+                    sum +=length;
+                    final int tmp=sum;
+                    bos.write(buf,0,length);
+                }
+                byte[] results=bos.toByteArray();
+                final Bitmap bmp= BitmapFactory.decodeByteArray(
+                        results,0,results.length
+                );
+
+                return bmp;
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
